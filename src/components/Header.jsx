8c83dbe39453f27e9d7b9bad5a700861276806e7
Researcher <70377003+abdulmoiz-bit@users.noreply.github.com>
2023-08-20 22:56:48 +0500

    remove work

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -29,7 +29,6 @@ const Header = () => {
             <div className='menu-list' data-aos="fade-left">
                 <ul>
                     <li><NavLink to="/">Home</NavLink></li>
-                    <li><a href="#portfolio">Work</a></li>
                     <li><NavLink to="/about">About</NavLink></li>
                     <li><NavLink to="/contact">Contact</NavLink></li>
                 </ul>
